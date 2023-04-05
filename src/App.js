3c608eb68ce2a1151cb322e81b8c46335bf43cd0
minwachya <minwachya@gmail.com>
2023-04-05 12:42:45 +0900

    [comment] 주석 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className='App'>
       <Todo />
+      <Todo /> {/* 주석은 이렇게 답니다... 꽤나 복잡하군 */}
+      {
+        // 한줄 주석은 요로코롬.
+      }
     </div>
   );
 }
