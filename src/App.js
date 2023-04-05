3c87da3ab2cb42b67a763f1c484ee50873183b0c
minwachya <minwachya@gmail.com>
2023-04-05 13:04:42 +0900

    [modify] App을 func에서 class로 변경

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
 import Todo from './Todo';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <Todo />
-      <Todo /> {/* 주석은 이렇게 답니다... 꽤나 복잡하군 */}
-      {
-        // 한줄 주석은 요로코롬.
-      }
-    </div>
-  );
-}
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Todo />
+        <Todo /> {/* 주석은 이렇게 답니다... 꽤나 복잡하군 */}
+        {
+          // 한줄 주석은 요로코롬.
+        }
+      </div>
+      );
+    }
+  }
 
 export default App;
