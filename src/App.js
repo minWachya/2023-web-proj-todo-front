9eaed9d0259a764464324b4e71810f1775993e33
minwachya <minwachya@gmail.com>
2023-04-05 13:08:52 +0900

    [feat] 부모인인 App이 자식인 Todo에게 props 넘겨주기

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: {id:0, title: '맛있는 거 먹기', done: true},
+    };
+  }
+
   render() {
     return (
       <div className='App'>
-        <Todo />
-        <Todo /> {/* 주석은 이렇게 답니다... 꽤나 복잡하군 */}
+        <Todo item={this.state.item}/>
+        {/* 주석은 이렇게 답니다... 꽤나 복잡하군 */}
         {
           // 한줄 주석은 요로코롬.
         }
