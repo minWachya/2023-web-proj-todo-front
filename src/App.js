e456a18684497a3d181a325dfa5c6f576220db2d
minwachya <minwachya@gmail.com>
2023-04-10 15:54:37 +0900

    [feat] props로  더미데이터만들어서 Todo item 여러 개 생성

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    // 나중에 백엔드에서 받아 올 데이터. 지금 이건 더미 데이터임.
     this.state = {
-      item: {id:0, title: '맛있는 거 먹기', done: true},
+      items: [
+        {id:0, title: '맛있는 거 먹기', done: true}, 
+        {id:1, title: '푹 자기', done: true}, 
+        {id:2, title: '정처기 공부하기', done: false}]
     };
   }
 
   render() {
+    // Todo 컴포넌트 배열 생성
+    var todoItems = this.state.items.map(
+      (item, index) => (
+        <Todo item={item} />
+      )
+    );
+    
     return (
       <div className='App'>
-        <Todo item={this.state.item}/>
-        {/* 주석은 이렇게 답니다... 꽤나 복잡하군 */}
+        
+        {/* Todo 컴포넌트 여러 개 */}
+        {todoItems}
         {
           // 한줄 주석은 요로코롬.
         }
