1455d716e778d0aff7488bb5b0200102dcc26658
minwachya <minwachya@gmail.com>
2023-04-12 12:52:47 +0900

    [feat] AddTodo에 onInputChange 함수 추가

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -7,15 +7,28 @@ class AddTodo extends React.Component {
         this.state = {item: {title: ""}}; // 제목 담을 변수
     }
 
+    // 텍스트 입력할 때마다 호출: props의 item.title에 입력된 값 넣음
+    onInputChange = (event) => {
+        const thisItem = this.state.item;
+        thisItem.title = event.target.value;
+        this.setState({item: thisItem});
+        console.log(thisItem);
+    }
+
     render() {
         return (
             <Paper style={{margin: 16, padding: 16}}>
                 <Grid container>
                     <Grid xs={11} md={11} item style={{paddingRight: 16}}>
-                        <TextField placeholder="Add Todo here" fullWidth />
+                        <TextField 
+                        placeholder="Add Todo here"
+                        fullWidth
+                        onChange={this.onInputChange}
+                        value={this.state.item.title}
+                         />
                     </Grid>
                     <Grid xs={1} md={1} item>
-                        <Button fullWidth color="secondary" variant="outlined">+</Button>
+                        <Button fullWidth color="secondary" variant="contained">+</Button>
                     </Grid>
                 </Grid>
             </Paper>
