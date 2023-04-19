cb472db6b2e970cffb7e20fe50c5d8c98dc53d27
minwachya <minwachya@gmail.com>
2023-04-19 13:12:27 +0900

    [comment] app config 주석 추가

--- src/app-config.js
+++ src/app-config.js
@@ -1,8 +1,9 @@
 let backendHost;
 
-//
+// 현재 브라우저의 domain name 얻음
 const hostname = window && window.location && window.location.hostname;
 
+// 현재 브라우저의 domain name이 localhost면 아래 주소를 백엔드 서비스 주소로 설정
 if(hostname === "localhost") {
     backendHost = "http://localhost:8080";
 }
