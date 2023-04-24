bb1f457fb12e34e5844c70d5de7aa795d53c190f
minwachya <minwachya@gmail.com>
2023-04-24 15:28:50 +0900

    [feat] 실제 서버와 연결하여 아이템 생성, 삭제, 읽기 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import AddTodo from './AddTodo.js'
 import './App.css';
 import {Paper, List, Container} from "@material-ui/core";
+import { call } from "./service/ApiService";
 
 class App extends React.Component {
   constructor(props) {
@@ -20,53 +21,24 @@ class App extends React.Component {
   // CORS 오류 발생: 8080이 아닌 3000에서 요청 오므로 거절...(주소가 다르므로)
   // 이걸 허락해줘야 처음 리소스 제공항 도메인이 현재 요청하려는 도메인과 다르더라고 요청을 허락해줌
   componentDidMount() {
-    const requestOptions = {
-      method: "GET",
-      headers: {"Content-Type": "application/json"},
-    };
-
-    /* 
-      fetch 반환 타입 == Promise: 비동기 처리 결과를 담는 객체. 미래에 응답이 도착하면 어떯게 해라.
-      Promise 의 3가지 상태: 대기/이행/실패
-      1. 대기 Pending: Promise 객체 생성 시 초기 상태
-      2. 이행 Resolved: 요청 성공적으로 끝난 상태, 성공시 then에서 성공 함수 실행하고 이행상태 됨
-      3. 실패 Rejected: thend의 실패 함수 실행하고 실패 상태 됨. catch 이용해 오류 처리함
-    */
-   // 기존 todo 리스트 받아오기
-    fetch("http://localhost:8080/todo", requestOptions) // 이 옵션으로 요청 보낼 건데 처리할 수 있어?(작은 요청) => 진짜 요청 보냄
-      .then((response) => response.json()) // string인 response를 json으로 변환. 얘의 반환값도 Promise!: method chaining
-      .then( // then(cb1, cb2): cb1-성공 시 콜백 함수, cb2-실패 시 콜백 함수
-        (response) => {
-          this.setState({items: response.data,});
-        },
-        (error) => {
-          this.setState({error,});
-        }
-     );
-     // .catch(); 오류처리는 then(cb1, cb2)보단 catch 많이 이용
+    call("/todo", "GET", null).then((response) => 
+      this.setState({items: response.data})
+    );
   }
 
   // todo list에 데이터 추가하는 함수: AddTodo에 전달
   add = (item) => {
-    // todo 객체 생성
-    const thisItems = this.state.items;
-    item.id = "ID-" + thisItems.length;
-    item.done = false;
-    thisItems.push(item);
-    // 업데이트: setState통해 ui 다시 그림
-    this.setState({items: thisItems});
-    console.log("items: ",  this.state.items);
-  }
+    call("/todo", "POST", item).then((response) => 
+      this.setState({items: response.data})
+    );
+  };
   
   // todo list의 데이터 삭제하는 함수: Todo에 전달
   delete = (item) => {
-    const thisItems = this.state.items;
-    // 삭제할 id 외의 아이템만 newItems에 답기
-    const newItems = thisItems.filter(e => e.id !== item.id);
-    this.setState({items: newItems}, () => {
-      console.log("Update Items: ", this.state.items)
-    });
-  }
+    call("/todo", "DELETE", item).then((response) => 
+      this.setState({items: response.data})
+    );
+  };
 
 
   render() {
--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,5 +1,6 @@
 import { API_BASE_URL } from "../app-config"
 
+// Promise 객체 리턴
 export function call(api, method, request) {
     let options = {
         headers: new Headers({
@@ -8,11 +9,12 @@ export function call(api, method, request) {
         url: API_BASE_URL + api,
         method: method,
     };
-
+    // 요청 데이터 있으면 body에 담음
     if(request) {
-        options.body = JSON.stringify(request);
+        options.body = JSON.stringify(request); // 문자열로 변환
     }
 
+    // 서버에 요청 후 응답 받아옴. Promise 반환함
     return fetch(options.url, options).then((response) => 
         response.json().then((json) => {
             if(!response.ok) {
