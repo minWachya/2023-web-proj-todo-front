00a15773ddedd3c96e407f4b80d570c37e91c71e
minwachya <minwachya@gmail.com>
2023-05-29 15:38:31 +0900

    [chore] 배포한 서버 url로 base url 변경 및 프론트엔드 배포위한 애플리케이션 생성(eb init)

--- src/app-config.js
+++ src/app-config.js
@@ -5,7 +5,9 @@ const hostname = window && window.location && window.location.hostname;
 
 // 현재 브라우저의 domain name이 localhost면 아래 주소를 백엔드 서비스 주소로 설정
 if(hostname === "localhost") {
-    backendHost = "http://localhost:8080";
+//     backendHost = "http://localhost:8080";
+// } else {
+    backendHost = "http://prod-todo-backend-20190054.us-west-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
